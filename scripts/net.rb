5799d64315f1ff6b5e3bb6e4cecbded503e103b4
Davide De Rosa <keeshux@gmail.com>
2020-05-15 12:40:46 +0200

    Use semantic presets
    Drop redundant 128-GCM endpoints.

--- scripts/net.rb
+++ scripts/net.rb
@@ -11,7 +11,8 @@ load("util.rb")
 
 servers_html = File.open("../template/servers.html") { |f| Nokogiri::HTML(f) }
 ca = File.read("../static/ca.crt")
-tls_wrap = read_tls_wrap("auth", 1, "../static/ta.key", 4, 20)
+tls_auth = read_tls_wrap("auth", 1, "../static/ta.key", 4, 20)
+tls_crypt = read_tls_wrap("auth", 1, "../static/ta.key", 4, 20)
 domain = "secureconnect.me"
 #domain = "torguardvpnaccess.com"
 
@@ -29,74 +30,86 @@ servers.map! { |s|
 
 cfg = {
     ca: ca,
-    wrap: tls_wrap,
-    cipher: "AES-256-GCM",
-    frame: 2,
-    compression: 0,
     eku: true,
     ping: 5
 }
 
-cfg_sha1 = cfg.dup
-cfg_sha1["auth"] = "SHA1"
-cfg_sha1["ep"] = [
-    "UDP:443",
-    "UDP:80",
-    "UDP:995",
-    "TCP:443",
-    "TCP:80",
-    "TCP:995"
-]
-
-cfg_sha256 = cfg.dup
-cfg_sha256["auth"] = "SHA256"
-cfg_sha256["ep"] = [
+cfg_default = cfg.dup
+cfg_default["cipher"] = "AES-256-GCM"
+cfg_default["auth"] = "SHA256"
+cfg_default["ep"] = [
     "UDP:1912",
     "UDP:1195",
     "TCP:1912",
     "TCP:1195"
 ]
+cfg_default["wrap"] = tls_auth
+cfg_default["frame"] = 2
+cfg_default["compression"] = 0
 
-cfg_sha512 = cfg.dup
-cfg_sha512["auth"] = "SHA512"
-cfg_sha512["ep"] = [
+cfg_strong = cfg.dup
+cfg_strong["cipher"] = "AES-256-GCM"
+cfg_strong["auth"] = "SHA512"
+cfg_strong["ep"] = [
     "UDP:1215",
     "UDP:389",
     "TCP:1215",
     "TCP:389"
 ]
+cfg_strong["wrap"] = tls_auth
+cfg_strong["frame"] = 2
+cfg_strong["compression"] = 0
+
+cfg_comp = cfg.dup
+cfg_comp["cipher"] = "AES-128-CBC"
+cfg_comp["auth"] = "SHA256"
+cfg_comp["ep"] = [
+    "UDP:53",
+    "TCP:53"
+]
+cfg_comp["wrap"] = tls_crypt
+cfg_comp["frame"] = 1
+cfg_comp["compression"] = 1
+
+#cfg_sha1 = cfg.dup
+#cfg_sha1["cipher"] = "AES-128-GCM"
+#cfg_sha1["auth"] = "SHA1"
+#cfg_sha1["ep"] = [
+#    "UDP:995",
+#    "TCP:995"
+#]
 
 external = {
     hostname: "${id}.#{domain}"
 }
 
-preset1 = {
-    id: "preset1",
-    name: "SHA1",
-    comment: "160-bit authentication",
-    cfg: cfg_sha1,
+preset_default = {
+    id: "preset-default",
+    name: "Default",
+    comment: "256-bit encryption / 256-bit auth",
+    cfg: cfg_default,
     external: external
 }
-preset256 = {
-    id: "preset256",
-    name: "SHA256",
-    comment: "256-bit authentication",
-    cfg: cfg_sha256,
+preset_strong = {
+    id: "preset-strong",
+    name: "Strong",
+    comment: "256-bit encryption / 512-bit auth",
+    cfg: cfg_strong,
     external: external
 }
-preset512 = {
-    id: "preset512",
-    name: "SHA512",
-    comment: "512-bit authentication",
-    cfg: cfg_sha512,
+preset_comp = {
+    id: "preset-comp",
+    name: "Compatible",
+    comment: "128-bit encryption / 256-bit auth",
+    cfg: cfg_comp,
     external: external
 }
-presets = [preset1, preset256, preset512]
+presets = [preset_default, preset_comp, preset_strong]
 
 defaults = {
     :username => "user@mail.com",
     :pool => "us",
-    :preset => "preset1"
+    :preset => "preset-default"
 }
 
 ###
