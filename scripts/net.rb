a0c1df0ef63dddb85b3e9c506561d51da1d56853
Davide De Rosa <keeshux@gmail.com>
2020-05-15 16:34:53 +0200

    Simplify/fix presets

--- scripts/net.rb
+++ scripts/net.rb
@@ -12,7 +12,7 @@ load("util.rb")
 servers_html = File.open("../template/servers.html") { |f| Nokogiri::HTML(f) }
 ca = File.read("../static/ca.crt")
 tls_auth = read_tls_wrap("auth", 1, "../static/ta.key", 4, 20)
-tls_crypt = read_tls_wrap("auth", 1, "../static/ta.key", 4, 20)
+tls_crypt = read_tls_wrap("crypt", 1, "../static/ta.key", 4, 20)
 domain = "secureconnect.me"
 #domain = "torguardvpnaccess.com"
 
@@ -30,86 +30,63 @@ servers.map! { |s|
 
 cfg = {
     ca: ca,
+    frame: 1,
+    compression: 1,
     eku: true,
     ping: 5
 }
 
 cfg_default = cfg.dup
-cfg_default["cipher"] = "AES-256-GCM"
-cfg_default["auth"] = "SHA256"
+cfg_default["cipher"] = "AES-128-GCM"
+cfg_default["auth"] = "SHA1"
 cfg_default["ep"] = [
-    "UDP:1912",
-    "UDP:1195",
-    "TCP:1912",
-    "TCP:1195"
+    "UDP:80",
+    "UDP:443",
+    "UDP:995",
+    "TCP:80",
+    "TCP:443",
+    "TCP:995"
 ]
-cfg_default["wrap"] = tls_auth
-cfg_default["frame"] = 2
-cfg_default["compression"] = 0
 
 cfg_strong = cfg.dup
 cfg_strong["cipher"] = "AES-256-GCM"
-cfg_strong["auth"] = "SHA512"
+cfg_strong["auth"] = "SHA256"
 cfg_strong["ep"] = [
-    "UDP:1215",
-    "UDP:389",
-    "TCP:1215",
-    "TCP:389"
-]
-cfg_strong["wrap"] = tls_auth
-cfg_strong["frame"] = 2
-cfg_strong["compression"] = 0
-
-cfg_comp = cfg.dup
-cfg_comp["cipher"] = "AES-128-CBC"
-cfg_comp["auth"] = "SHA256"
-cfg_comp["ep"] = [
     "UDP:53",
-    "TCP:53"
+    "UDP:501",
+    "UDP:1198",
+    "UDP:9201",
+    "TCP:53",
+    "TCP:501",
+    "TCP:1198",
+    "TCP:9201"
 ]
-cfg_comp["wrap"] = tls_crypt
-cfg_comp["frame"] = 1
-cfg_comp["compression"] = 1
-
-#cfg_sha1 = cfg.dup
-#cfg_sha1["cipher"] = "AES-128-GCM"
-#cfg_sha1["auth"] = "SHA1"
-#cfg_sha1["ep"] = [
-#    "UDP:995",
-#    "TCP:995"
-#]
+cfg_strong["wrap"] = tls_crypt
 
 external = {
     hostname: "${id}.#{domain}"
 }
 
 preset_default = {
-    id: "preset-default",
+    id: "default",
     name: "Default",
-    comment: "256-bit encryption / 256-bit auth",
+    comment: "128-bit encryption",
     cfg: cfg_default,
     external: external
 }
 preset_strong = {
-    id: "preset-strong",
+    id: "strong",
     name: "Strong",
-    comment: "256-bit encryption / 512-bit auth",
+    comment: "256-bit encryption",
     cfg: cfg_strong,
     external: external
 }
-preset_comp = {
-    id: "preset-comp",
-    name: "Compatible",
-    comment: "128-bit encryption / 256-bit auth",
-    cfg: cfg_comp,
-    external: external
-}
-presets = [preset_default, preset_comp, preset_strong]
+presets = [preset_default, preset_strong]
 
 defaults = {
     :username => "user@mail.com",
     :pool => "us",
-    :preset => "preset-default"
+    :preset => "default"
 }
 
 ###
