93b752a8a24e4f9bcacf6063cba38b96afa6e919
Davide De Rosa <keeshux@gmail.com>
2021-04-23 01:09:31 +0200

    Look for weird errors

--- update-servers.sh
+++ update-servers.sh
@@ -5,6 +5,9 @@ SERVERS_DST="$TPL/servers.html"
 
 mkdir -p $TPL
 if ! curl -L $URL >$SERVERS_DST.tmp; then
-    exit
+    exit 1
+fi
+if grep "1020" $SERVERS_DST.tmp; then
+    exit 1
 fi
 mv $SERVERS_DST.tmp $SERVERS_DST
